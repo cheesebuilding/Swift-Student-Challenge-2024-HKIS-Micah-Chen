e02cdb4258c11a524a1ce46273735aa2e0d26d1f
cheesebuilding <micahchen004@gmail.com>
2024-02-18 12:24:57 +0800

    Added FinalView and adjustments

---
+++ SwiftStudentChallenge/FinalView.swift
@@ -0,0 +1,18 @@
+//
+//  FinalView.swift
+//  SwiftStudentChallenge
+//
+//  Created by Micah Chen on 2/18/24.
+//
+
+import SwiftUI
+
+struct FinalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinalView()
+}
